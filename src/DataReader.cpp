6d316089f1d2b7ed90ed455256a7dcced9906563
diogo <diogo.campeao@gmail.com>
2025-03-17 21:34:52 +0000

    Datareader readLocations added, printing only the values for now

--- src/DataReader.cpp
+++ src/DataReader.cpp
@@ -10,12 +10,51 @@ using namespace std;
 
 DataReader::DataReader() {}
 
-void DataReader::readDistances(const std::string fileName) {
+DataReader::~DataReader() {}
+
+
+void DataReader::readLocations(const std::string& fileName) {
+    ifstream file(fileName);
+    if (!file) {
+        cerr << "Error opening file " << fileName << endl;
+        exit(1);
+    }
+
+    string name, idStr, code, parkingStr;
+    int id;
+    bool parking;
+
+    string line;
+    getline(file, line);
+
+    char delimiter = ',';
+
+    while (getline(file, line)) {
+        istringstream iss(line);
+
+        getline(iss, name, delimiter);
+        getline(iss, idStr, delimiter);
+        getline(iss, code, delimiter);
+        getline(iss, parkingStr, '\r');
+
+        id = stoi(idStr);
+        parking = parkingStr == "1";
+
+        cout << "name: " << name << endl;
+        cout << "id: " << id << endl;
+        cout << "code: " << code << endl;
+        cout << "parking: " << (parking ? "Yes" : "No") << endl;
+        cout << endl;
+    }
+
+    cout << "File read. " << endl;
+    file.close();
+}
+
+void DataReader::readDistances(const std::string& fileName) {
     // TODO
 }
 
-void DataReader::readLocations(const std::string fileName) {
-    // TODO
-}
+
 
 
--- src/Menu.cpp
+++ src/Menu.cpp
@@ -6,12 +6,15 @@
 #include <fstream>
 #include <sstream>
 
+#include "../headers/DataReader.h"
+
 using namespace std;
 
 Menu::Menu() = default;
 
 void Menu::DefaultMenu() {
     int option;
+    DataReader reader = DataReader();
 
     do {
         cout << "\nIndividual Route Planning Tool\n"
@@ -22,7 +25,7 @@ void Menu::DefaultMenu() {
         cin >> option;
         switch (option) {
             case 1:
-                cout << "WIP" << endl;
+                reader.readLocations("../docs/Locations.csv");
                 break;
             case 2:
                 cout << "WIP" << endl;
