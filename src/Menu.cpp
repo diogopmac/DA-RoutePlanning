39bf7852f94d42b91ae0592f6673e4ffa31e6895
diogo <diogo.campeao@gmail.com>
2025-03-25 09:47:25 +0000

    Unnecessary avoid_nodes removed

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -133,7 +133,6 @@ void Menu::DefaultMenu() {
                 int source, destination;
                 vector<int> res;
                 vector<int> res2;
-                vector<int> avoid_nodes;
                 vector<pair<int,int>> avoid_edges;
 
                 cout << "Enter mode: "; cin >> mode;
@@ -164,10 +163,10 @@ void Menu::DefaultMenu() {
                 cout << "(" << graph.findVertex(destination)->getDist() << ")" << endl;
 
                 if (mode == "driving") {
-                    res2 = bestPath(&graph, source, destination, "drivable", true, avoid_nodes, avoid_edges);
+                    res2 = bestPath(&graph, source, destination, "drivable", true, {}, avoid_edges);
                 }
                 else if (mode == "walking") {
-                    res2 = bestPath(&graph, source, destination, "walkable", true, avoid_nodes, avoid_edges);
+                    res2 = bestPath(&graph, source, destination, "walkable", true, {}, avoid_edges);
                 }
 
                 cout << "Source:" << graph.findVertex(source)->getID() << endl;
